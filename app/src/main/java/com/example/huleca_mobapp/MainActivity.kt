7250424921670af028c34ea67d2ccb16724225c8
wehejojo <jojoliwag4@gmail.com>
2025-03-07 15:18:08 +0800

    test commit >:)

--- app/src/main/java/com/example/huleca_mobapp/MainActivity.kt
+++ app/src/main/java/com/example/huleca_mobapp/MainActivity.kt
@@ -13,9 +13,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -71,7 +68,7 @@ fun HomeScreen(navController: NavController) {
 
 @Composable
 fun DetectedViolations(){
-  Text(text = "Detected Violations")
+  Text(text = "Detected Violations ( Test Change >:) )")
 }
 
 @Composable
